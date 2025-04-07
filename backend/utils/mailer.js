3d8ea059e9d440ff9e935d609d2364853f330377
Wiltord <wiltordichingwa@gmail.com>
2025-04-07 15:29:21 +0300

    url update

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
 });
 
 async function sendResetEmail(email, token) {
-  const resetLink = `http://localhost:5000/reset.html?token=${token}`;
+  const resetLink = `https://bank-management-system-0udz.onrender.com/public/reset.html?token=${token}`;
   try {
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
